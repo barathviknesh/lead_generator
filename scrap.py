1c1ab22498c1c43961ffb13f161b180d312737c8
barathviknesh <barathviknesh.9566@gmail.com>
2020-07-10 11:32:40 +0530

    initial2

--- scrap.py
+++ scrap.py
@@ -2,8 +2,6 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup
 
-# get url as input and return the html content (table)
-
 
 def get_webpage(url):
     r = requests.get(url)
@@ -14,6 +12,14 @@ def get_webpage(url):
 
 
 url = "http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.html"
+
+# r = requests.get(url)
+
+# html_content = r.text
+
+# soup = BeautifulSoup(html_content, "html.parser")
+# tables = soup.find('table', {'class': 'table100'})
+# print(tables)
 
 
 print(get_webpage(url))
